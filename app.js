0677d2f2270da236985830501732644f74e1e4a9
Lee Soojin <76468868+Lee-Soojin@users.noreply.github.com>
2024-03-17 16:43:22 +0900

    feat: Create video, screenshot, duplicated folder

--- app.js
+++ app.js
@@ -9,3 +9,11 @@ if (!folder || !fs.existsSync(dirPath)) {
   console.log("Pictures 폴더 내의 폴더명을 입력해주세요.");
   return;
 }
+
+const videoDir = path.join(dirPath, "video");
+const screenshotDir = path.join(dirPath, "screenshot");
+const duplicatedDir = path.join(dirPath, "duplicated");
+
+!fs.existsSync(videoDir) && fs.mkdirSync(videoDir);
+!fs.existsSync(screenshotDir) && fs.mkdirSync(screenshotDir);
+!fs.existsSync(duplicatedDir) && fs.mkdirSync(duplicatedDir);
